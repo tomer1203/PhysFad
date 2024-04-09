2ad5c47eab6252ea99c0b98d97df7b9a21d4765a
Tomer_Shaked <tomer1shaked@gmail.com>
2024-04-10 01:58:04 +0300

    finishing merge process

--- ChannelMatrixEvaluation.m
+++ ChannelMatrixEvaluation.m
@@ -40,107 +40,12 @@ gamma_env = 0*ones(size(x_env));% 100*ones
 load('ExampleRIS3.mat','x_ris','y_ris');
 % x_ris = x_ris(1,1:1);%1,1:25
 % y_ris = y_ris(1,1:1);%1,1:25
-
 % properties
 ris_num_samples = 200;
 if length(x_ris)~=length(y_ris)
     disp('Error: x_ris and y_ris do not have the same length.');
 else
     N_RIS = length(x_ris);
-end
-
-
-fres_ris_ON = 1*ones(ris_num_samples,1);
-fres_ris_OFF = 5*ones(ris_num_samples,1);
-
-%--- Yotam and Ron's Values ---%
-% chi_ris = 6*ones(size(x_ris));
-% gamma_ris = unifrnd(0,100,size(x_ris));
-% fres_ris   = unifrnd(1,5,ris_num_samples,N_RIS);
-
-%--- Sin distribution for chi and gamma ---%
-% chi_phase=2*pi*rand(1,ris_num_samples);
-% chi_phase=2*pi*zeros(1,ris_num_samples);
-chi_phase=2*pi*linspace(0,1,ris_num_samples);
-chi_frequency=0.3*rand(1,ris_num_samples);
-% chi_frequency=0.3*ones(1,ris_num_samples);
-% chi_ris = 50+50*sin(chi_frequency'*[1:size(x_ris,2)]+chi_phase'*ones(size(x_ris)));
-% chi_ris = 25*rand(ris_num_samples,1)+25*rand(ris_num_samples,1).*sin(chi_frequency'*[1:size(x_ris,2)]+chi_phase'*ones(size(x_ris)));
-% gamma_ris = 0.01+0.01*sin(chi_frequency'*[1:size(x_ris,2)]+chi_phase'*ones(size(x_ris)));
-% fres_ris   = 1+0.1*sin(chi_frequency'*[1:size(x_ris,2)]+chi_phase'*ones(size(x_ris)));
-
-%--- Normal distribution ---%
-% gamma_ris = zeros(cat(2,ris_num_samples,size(x_ris)));
-% chi_ris = zeros(cat(2,ris_num_samples,size(x_ris)));
-% gamma_mu_linespace=linspace(-1.5,1.5,ris_num_samples);
-% gamma_std_linespace=linspace(0.3,3,ris_num_samples);
-% for J=1:ris_num_samples
-%     gamma_ris(J,:) = normpdf(linspace(-1.5,1.5,N_RIS),0,gamma_std_linespace(J))';
-%     chi_ris(J,:) = normpdf(linspace(-1.5,1.5,N_RIS),0,gamma_std_linespace(J))';
-% end
-% gamma_ris(isnan(gamma_ris))=0;
-% chi_ris(isnan(chi_ris))=0;
-
-%--- uniform random ---%
-chi_ris = unifrnd(0,15,cat(2,ris_num_samples,size(x_ris))); %0-100
-gamma_ris = unifrnd(0,10,cat(2,ris_num_samples,size(x_ris))); %0-100
-fres_ris   = unifrnd(1,5,ris_num_samples,N_RIS);
-
-
-%--- constants ---%
-% chi_ris = 6*ones(cat(2,ris_num_samples,size(x_ris)));
-% gamma_ris = 0.2*ones(cat(2,ris_num_samples,size(x_ris)));%50
-% fres_ris = ones(cat(2,ris_num_samples,size(x_ris)));
-
-%% Testing Parameter Validity
-if length(x_tx)~=length(y_tx)
-    disp('Error: x_tx and y_tx do not have the same length.');
-else
-    N_T = length(x_tx);
-end
-if length(fres_tx)~=N_T
-    disp('Error: x_tx and fres_tx do not have the same length.');
-end
-if length(chi_tx)~=N_T
-    disp('Error: x_tx and chi_tx do not have the same length.');
-end
-if length(gamma_tx)~=N_T
-    disp('Error: x_tx and gamma_tx do not have the same length.');
-end
-if length(x_rx)~=length(y_rx)
-    disp('Error: x_rx and y_rx do not have the same length.');
-else
-    N_R = length(x_rx);
-end
-if length(fres_rx)~=N_R
-    disp('Error: x_rx and fres_rx do not have the same length.');
-end
-if length(chi_rx)~=N_R
-    disp('Error: x_rx and chi_rx do not have the same length.');
-end
-if length(gamma_rx)~=N_R
-    disp('Error: x_rx and gamma_rx do not have the same length.');
-end
-if length(x_env)~=length(y_env)
-    disp('Error: x_env and y_env do not have the same length.');
-else
-    N_E = length(x_env);
-end
-if length(fres_env)~=N_E
-    disp('Error: x_env and fres_env do not have the same length.');
-end
-if length(chi_env)~=N_E
-    disp('Error: x_env and chi_env do not have the same length.');
-end
-if length(gamma_env)~=N_E
-    disp('Error: x_env and gamma_env do not have the same length.');
-end
-
-if length(chi_ris(1,:))~=N_RIS
-    disp('Error: x_ris and chi_ris do not have the same length.');
-end
-if length(gamma_ris(1,:))~=N_RIS
-    disp('Error: x_ris and gamma_ris do not have the same length.');
 end
 
 
@@ -218,53 +123,68 @@ end
 avg_sampled_Hs = zeros(ris_num_samples,length(freq));
 sampled_Hs = zeros(ris_num_samples,length(freq),length(x_rx),length(x_tx));
 RISConfiguration = zeros(ris_num_samples,length([fres_ris(1,:),chi_ris(1,:),gamma_ris(1,:)]));
+%% Debug
+% load('new_full_RISConfiguration.mat','RISConfiguration');
+% fres_ris = RISConfiguration(:,1:length(x_ris));
+% chi_ris = RISConfiguration(:,length(x_ris)+1:2*length(x_ris));
+% gamma_ris = RISConfiguration(:,2*length(x_ris)+1:3*length(x_ris));
+%% Debug End
 SumRateVec = zeros(ris_num_samples,1);
-for sample=1:ris_num_samples
-    % RIS Configuration
-    % config_ris = round(rand(1,N_RIS));
-    % clear fres_ris;
-    % for cc=1:length(config_ris)
-    %     if config_ris(cc)==0
-    %         fres_ris(cc) = fres_ris_OFF(sample);
-    %     elseif config_ris(cc)==1
-    %         fres_ris(cc) = fres_ris_ON(sample);
-    %     end
-    % end
-    % if length(fres_ris)~=N_RIS
-    %     disp('Error: x_ris and fres_ris do not have the same length.');
-    % end
+ppm = ParforProgressbar(ris_num_samples);
+W = Calc_W(freq,x_tx,y_tx,x_rx,y_rx, x_env,y_env,x_ris,y_ris);
+parfor sample=1:ris_num_samples
+    
     disp(sample);
-%     [~,H] = getH(freq,...
-%                     x_tx,y_tx,fres_tx,chi_tx,gamma_tx,...
-%                     x_rx,y_rx,fres_rx,chi_rx,gamma_rx,...
-%                     x_env,y_env,fres_env,chi_env,gamma_env,...
-%                     x_ris,y_ris,fres_ris(sample,:),chi_ris(sample,:),gamma_ris(sample,:));
-    [~,H] = getH2(freq,...
-                    x_tx,y_tx,fres_tx,chi_tx,gamma_tx,...
-                    x_rx,y_rx,fres_rx,chi_rx,gamma_rx,...
-                    x_env,y_env,fres_env,chi_env,gamma_env,...
-                    x_ris,y_ris,fres_ris(sample,:),chi_ris(sample,:),gamma_ris(sample,:));
-%     disp(H2==H);
-    avg_sampled_Hs(sample,:) = mean(mean(abs(H),2),3);
-    sampled_Hs(sample,:,:,:) = H;
+    % [~,H] = getH(freq,...
+    %                 x_tx,y_tx,fres_tx,chi_tx,gamma_tx,...
+    %                 x_rx,y_rx,fres_rx,chi_rx,gamma_rx,...
+    %                 x_env,y_env,fres_env,chi_env,gamma_env,...
+    %                 x_ris,y_ris,fres_ris(sample,:),chi_ris(sample,:),gamma_ris(sample,:));
+    % [~,H2] = getH2(freq,...
+    %                 x_tx,y_tx,fres_tx,chi_tx,gamma_tx,...
+    %                 x_rx,y_rx,fres_rx,chi_rx,gamma_rx,...
+    %                 x_env,y_env,fres_env,chi_env,gamma_env,...
+    %                 x_ris,y_ris,fres_ris(sample,:),chi_ris(sample,:),gamma_ris(sample,:));
+    [~,H] = getH3(freq,W,...
+        fres_tx,chi_tx,gamma_tx,...
+        fres_rx,chi_rx,gamma_rx,...
+        fres_env,chi_env,gamma_env,...
+        fres_ris(sample,:),chi_ris(sample,:),gamma_ris(sample,:));
+    % disp(H3==H);
+    % avg_sampled_Hs(sample,:) = mean(mean(abs(H),2),3);
+    avg_sampled_Hs(sample,:) = abs(H(:,1,2));
+    sampled_Hs(sample,:,:,:) = abs(H);
     SumRateVec(sample) = getSumRate(H,ones(length(H)),1);
     RISConfiguration(sample,:) = [fres_ris(sample,:),chi_ris(sample,:),gamma_ris(sample,:)];
+    ppm.increment();
 %     sampled_Hs(sample,:) = abs(H(:,1,1));
 end
+delete(ppm);
 plot(avg_sampled_Hs')
-figure;
-plot(SumRateVec);
-similarity_matrix = (1/101) * avg_sampled_Hs * avg_sampled_Hs'/mean(avg_sampled_Hs(:).^2);
-disp(min(similarity_matrix(:)));
-[dataDiversity,~,dataSampleDiv]= getDataDiversity(abs(sampled_Hs));
+title("Channel H between first transmitor to second reciever")
+xlabel("freq");
+% figure;
+% plot(std(avg_sampled_Hs,0,1))
+% xlabel("freq");
+% ylabel("std(|H_{12}|)");
+% figure;
+% plot(SumRateVec);
+% xlabel("test x");
+% ylabel("channel sum rate");
+% similarity_matrix = (1/101) * avg_sampled_Hs * avg_sampled_Hs'/mean(avg_sampled_Hs(:).^2);
+% disp(min(similarity_matrix(:)));
+[dataDiversity,~,dataSampleDiv]= getDataDiversity(sampled_Hs);
 figure;
 plot(dataSampleDiv);
 disp('diversity '+string(dataDiversity));
-mean_H = mean(avg_sampled_Hs);
-mean_similarity = (1/101) * mean_H * avg_sampled_Hs'/mean(mean_H(:).^2);
-figure;
-plot(mean_similarity*RISConfiguration);
-
+figure();
+plot(SumRateVec)
+disp('average rate'+string(mean(SumRateVec)))
+disp('max rate'+string(max(SumRateVec)))
+% mean_H = mean(avg_sampled_Hs);
+% mean_similarity = (1/101) * mean_H * avg_sampled_Hs'/mean(mean_H(:).^2);
+% figure;
+% plot(mean_similarity*RISConfiguration);
 % plot(RISConfiguration(:,70)',mean_similarity,'*')
 % plot(avg_sampled_Hs(abs(mean_similarity-1)>0.2,:)','LineWidth',1,'LineStyle',':')
 % hold on
@@ -272,8 +192,6 @@ plot(mean_similarity*RISConfiguration);
 % csvwrite("H_realizations.txt",avg_sampled_Hs(abs(mean_similarity-1)>0.3,:));
 % csvwrite("H_residuals.txt",avg_sampled_Hs-mean_H);
 % csvwrite("RISConfiguration.txt",RISConfiguration(abs(mean_similarity-1)>0.3,:));
-
 % save("new_full_H_realizations.mat",'sampled_Hs');
 % csvwrite("H_residuals.txt",avg_sampled_Hs-mean_H);
 % save("new_full_RISConfiguration.mat",'RISConfiguration');
-
